bf5793d64aeaa8f5903bbcf876d6b43c84ceb287
SANJU <89297664+LolanBot18@users.noreply.github.com>
2021-12-01 20:00:17 +0530

    Update thunder.py

--- plugins/admemes/thunder.py
+++ plugins/admemes/thunder.py
@@ -6,22 +6,22 @@ from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
-DART_E_MOJI = "🎯"
+THUNDER_E_MOJI = "⚡"
 # EMOJI CONSTANTS
 
 
 @Client.on_message(
-    filters.command(["throw", "dart"], COMMAND_HAND_LER) &
+    filters.command(["thunder"], COMMAND_HAND_LER) &
     f_onw_fliter
 )
 async def throw_dart(client, message):
-    """ /throw an @AnimatedDart """
+    """ /thunder an @AnimatedThunder """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
     await client.send_dice(
         chat_id=message.chat.id,
-        emoji=DART_E_MOJI,
+        emoji=THUNDER_E_MOJI,
         disable_notification=True,
         reply_to_message_id=rep_mesg_id
     )
